211e33ceeb96979ede4b8b2788e612c0fe2f4d05
sshreya007 <shreyakhadka007@gmail.com>
2025-12-27 11:00:18 +0545

    Working on Navbar

---
+++ components/Navbar.tsx
@@ -0,0 +1,21 @@
+import Link from "next/link";
+
+export default function Navbar() {
+  return (
+    <nav className="navbar">
+      <div className="logo">TRIP</div>
+
+      <ul className="nav-links">
+        <li>Home</li>
+        <li>Packages</li>
+        <li>Explore</li>
+        <li>About</li>
+        <li>Contact us</li>
+      </ul>
+
+      <Link href="/login" className="login-btn">
+        Login / Sign Up
+      </Link>
+    </nav>
+  );
+}
