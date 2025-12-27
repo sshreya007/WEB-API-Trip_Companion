af742043ec07a199a3d97111f7492acbaf6339da
sshreya007 <shreyakhadka007@gmail.com>
2025-12-27 11:35:07 +0545

    Working on LoginPage

---
+++ app/auth/login/page.tsx
@@ -0,0 +1,18 @@
+import LoginForm from "@/components/LoginForm";
+import "@/styles/auth.css";
+
+export default function LoginPage() {
+  return (
+    <div className="auth-container">
+      <div className="auth-card">
+        <img src="/images/login.jpg" className="auth-image" />
+
+        <div className="auth-form">
+          <h2>Welcome</h2>
+          <p>Don’t have an account? <a href="/register">Sign Up</a></p>
+          <LoginForm />
+        </div>
+      </div>
+    </div>
+  );
+}
