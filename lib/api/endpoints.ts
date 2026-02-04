ff50df2eb1754b3e4e4bd46646f0826be78b4894
sshreya007 <shreyakhadka007@gmail.com>
2026-02-04 21:25:45 +0545

    Working on endpoints

--- lib/api/endpoints.ts
+++ lib/api/endpoints.ts
@@ -1,11 +1,25 @@
 //List of api routes
 //Single source of truth for api endpoints
 
-export const API={
-    AUTH:{
-        LOGIN: 'api/auth/login',
-        REGISTER:'api/auth/register',
-    }
+// export const API={
+//     AUTH:{
+//         LOGIN: 'api/auth/login',
+//         REGISTER:'api/auth/register',
+//     }
 
-}
+// }
 
+export const API_ENDPOINTS = {
+  AUTH: {
+    REGISTER: '/auth/register',
+    LOGIN: '/auth/login',
+  },
+  ADMIN: {
+    USERS: '/admin/users',
+    USER_BY_ID: (id: string) => `/admin/users/${id}`,
+  },
+  PROFILE: {
+    GET: '/profile',
+    UPDATE: (id: string) => `/profile/${id}`,
+  },
+};
